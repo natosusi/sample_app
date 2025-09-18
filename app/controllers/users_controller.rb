adcfbc01d70a66c5e3052c27415a66796a3c25f9
natsuki-saito <148896741+natosusi@users.noreply.github.com>
2025-09-18 18:44:46 +0000

    7章完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
   def new
-    debugger
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
-    debugger
+  end
+
+  def create
+    # マスアサインメント使用 @user = User.new(params[:user])
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_comfirmation)
   end
 end
